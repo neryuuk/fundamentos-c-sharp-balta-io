2a543845b66a301aca1122f2ce4e57ca055b20aa
Nelson Antunes <neryuuk@neryuuk.com>
2023-08-31 09:19:02 -0300

    switch Teste for MeuApp

---
+++ MeuApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MeuApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      const string text = "Hell-o, World!";
+      Console.WriteLine(text);
+    }
+  }
+}
