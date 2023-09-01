eea76ffd6a068bd5b4d7ca4e4b264d094a1fae73
Nelson Antunes <neryuuk@neryuuk.com>
2023-08-31 21:56:07 -0300

    testes de metodos

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -6,8 +6,19 @@ namespace MeuApp
   {
     static void Main(string[] _)
     {
+      MeuMetodo();
+      Console.WriteLine(RetornaNome("Nelson", "Antunes"));
+    }
+
+    static void MeuMetodo()
+    {
       string text = "Hell-o, World!";
       Console.WriteLine(text);
+    }
+
+    static string RetornaNome(string nome, string sobrenome, int? idade = null)
+    {
+      return $"{nome} {sobrenome}";
     }
   }
 }
