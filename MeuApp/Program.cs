715a7ef0e55dd3fe1055ba98dabc554a3f3cf13a
Nelson Antunes <neryuuk@neryuuk.com>
2023-09-01 19:29:14 -0300

    product struct

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -18,7 +18,27 @@ namespace MeuApp
 
     static string RetornaNome(string nome, string sobrenome, int? idade = null)
     {
+      Product mouse = new Product(1, "Mouse Gamer", 219.95);
       return $"{nome} {sobrenome}";
+    }
+  }
+
+  struct Product
+  {
+    public int Id;
+    public string Title;
+    public double Price;
+
+    public Product(int id, string title, double price)
+    {
+      Id = id;
+      Title = title;
+      Price = price;
+    }
+
+    public readonly double PriceInDollar(double dollar)
+    {
+      return Price * dollar;
     }
   }
 }
