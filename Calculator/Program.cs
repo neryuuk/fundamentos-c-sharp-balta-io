24a7287e4ae5e855f71311917fe69dfdab9f2361
Nelson Antunes <966712+neryuuk@users.noreply.github.com>
2023-09-04 14:32:29 -0300

    calculator application

---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calculator
+{
+  class Program
+  {
+    static void Main(string[] _)
+    {
+      Soma();
+    }
+
+    static float LerValor(string prompt, bool clear = false)
+    {
+      if (clear) Console.Clear();
+
+      Console.WriteLine(prompt);
+      return float.Parse(Console.ReadLine().Replace(',', '.'));
+    }
+
+    static void Soma()
+    {
+      float valor1 = LerValor("Primeiro valor:", true);
+      float valor2 = LerValor("Segundo valor:");
+      Console.WriteLine();
+      Console.WriteLine($"A soma entre {valor1} e {valor2} é {valor1 + valor2}");
+      Console.ReadKey();
+    }
+  }
+}
